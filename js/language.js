5e18055a762b36e16efaae2d244bf5e9392034eb
Christoph Zirkelbach <christoph@tigion.de>
2022-01-31 09:52:02 +0100

    Fix language detection for long and upper case language codes ('de-DE' -> 'de')

--- js/language.js
+++ js/language.js
@@ -2,14 +2,16 @@
 function initLanguage() {
     // get browser/system language setting
     let language = window.navigator.language || window.navigator.userLanguage;
+    // limit language code (2 chars, lower case)
+    language = language.slice(0, 2).toLowerCase();
     // set language
     setLanguage(language)
 }
 
 // set document language
 function setLanguage(language) {
-    // check supported languages
-    switch (language) {
+    // check supported languages (lower case)
+    switch (language.toLowerCase()) {
         case 'en': case 'de': break;
         default: language = 'en';
     }
